0b529370176c2c453ba8bb4abb28b4901eabfe61
DaCosySheeep <119451670+DaCosySheeep@users.noreply.github.com>
2024-05-25 13:53:51 +0100

    Cheese android 1.1 (equivalent to cheese 2.2)

--- app/src/main/java/me/dacosysheeep/cheese/MainActivity.java
+++ app/src/main/java/me/dacosysheeep/cheese/MainActivity.java
@@ -1,19 +1,11 @@
 package me.dacosysheeep.cheese;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.app.Fragment;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.google.android.material.navigation.NavigationView;
-
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,40 +21,43 @@ public class MainActivity extends AppCompatActivity {
 
         Button myButton=findViewById(R.id.showcheese);
         cheese=findViewById(R.id.cheese1);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    cheese.setVisibility(View.INVISIBLE);
-                    int r = random.nextInt(11) + 1;
-                    String imageName;
-                    String soundName;
 
-                    if (r == 11) {
-                        r = random.nextInt(11) + 1;
-                    }
-                    if (r == 11) {
-                        imageName = "potato";
-                        soundName = "imposter";
-                    } else {
-                        imageName = "cheese" + r;
-                        soundName = "cheese" + r;
-                    }
-                    int imageId = getResources().getIdentifier(imageName, "drawable", getPackageName());
-                    cheese.setImageResource(imageId);
-                    cheese.setVisibility(View.VISIBLE);
-                    if (mp != null) {
-                        mp.release();
-                    }
-                    int soundId = getResources().getIdentifier(soundName, "raw", getPackageName());
-                    mp = MediaPlayer.create(MainActivity.this, soundId);
-                    mp.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+
+        myButton.setOnClickListener(v -> showCheese());
+        View rootView=findViewById(android.R.id.content);
+        rootView.setOnClickListener(v -> showCheese());
+
+    }
+
+    private void showCheese() {
+        try {
+            cheese.setVisibility(View.INVISIBLE);
+            int r = random.nextInt(34) + 1;
+            String imageName;
+            String soundName;
+
+            if (r == 34) {
+                r = random.nextInt(34) + 1;
             }
-        });
-
+            if (r == 34) {
+                imageName = "potato";
+                soundName = "imposter";
+            } else {
+                imageName = "cheese" + r;
+                soundName = "cheese" + r;
+            }
+            int imageId = getResources().getIdentifier(imageName, "drawable", getPackageName());
+            cheese.setImageResource(imageId);
+            cheese.setVisibility(View.VISIBLE);
+            if (mp != null) {
+                mp.release();
+            }
+            int soundId = getResources().getIdentifier(soundName, "raw", getPackageName());
+            mp = MediaPlayer.create(MainActivity.this, soundId);
+            mp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
